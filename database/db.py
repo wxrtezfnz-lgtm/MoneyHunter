b0680dfc4738433b0c39d2128dc232d8cd70b12b
wxrtezfnz-lgtm <wxrtezfnz@gmail.com>
2026-07-24 22:29:37 +0300

    Fix database with income

--- database/db.py
+++ database/db.py
@@ -4,30 +4,32 @@ conn = sqlite3.connect("moneyhunter.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     telegram_id INTEGER,
     name TEXT,
     age TEXT,
     activity TEXT,
-    goal TEXT
+    goal TEXT,
+    income TEXT
 )
 """)
 
 conn.commit()
 
 
-def save_user(telegram_id, name, age, activity, goal):
+def save_user(telegram_id, name, age, activity, goal, income):
     cursor.execute("""
     INSERT INTO users
-    (telegram_id, name, age, activity, goal)
-    VALUES (?, ?, ?, ?, ?)
+    (telegram_id, name, age, activity, goal, income)
+    VALUES (?, ?, ?, ?, ?, ?)
     """, (
         telegram_id,
         name,
         age,
         activity,
-        goal
+        goal,
+        income
     ))
 
     conn.commit()
