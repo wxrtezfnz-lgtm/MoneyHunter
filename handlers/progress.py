e1d3c580a718b63aaf7ab6b5a29d61bc21e823bb
wxrtezfnz-lgtm <wxrtezfnz@gmail.com>
2026-07-25 23:04:24 +0300

    Auto update level

--- handlers/progress.py
+++ handlers/progress.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
 
-from database.db import get_day, next_day, add_xp
+from database.db import get_day, next_day, add_xp, update_level
 from services.day_builder import build_day
 from keyboards.progress_keyboard import progress_keyboard
 
@@ -28,6 +28,7 @@ async def next_day_callback(callback: CallbackQuery):
     # увеличиваем день
     next_day(callback.from_user.id)
     add_xp(callback.from_user.id, 50)
+    update_level(callback.from_user.id)
     day = get_day(callback.from_user.id)
 
     
